92452633b83950ac28a42c994d8cb2d3697d2835
gmb <gameboyt7@gmail.com>
2022-08-20 11:55:59 +0200

    Fixed boat and adventure search filter not working

--- web/pages/adventures/index.jsx
+++ web/pages/adventures/index.jsx
@@ -1,22 +1,24 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import AllEntities from '../../components/lists/AllEntities'
 import adventureService from '../../services/adventure'
+import { filterAndSortEntities } from '../../utils/listUtils'
 
 const Adventures = () => {
   const [adventures, setAdventures] = useState([])
   const [filter, setFilter] = useState('')
   const [filterProperty, setFilterProperty] = useState('address')
-  const sortFilterItems = ['Name', 'Address', 'Rating', 'Price']
   const [selectedTags, setSelectedTags] = useState([])
-
+  const [sortBy, setSortBy] = useState('')
+  const sortFilterItems = ['Name', 'Address', 'Rating', 'Price']
 
   useEffect(() => {
     adventureService.getAll().then((gotAdventures) => setAdventures(gotAdventures))
   }, [])
 
-  const adventuresToShow = filter
-    ? adventures.filter((adventure) => adventure[filterProperty].toLowerCase().includes(filter.toLowerCase()))
-    : adventures
+  const filteredAndSortedEntities = useMemo(
+    () => filterAndSortEntities(adventures, filter, filterProperty, selectedTags, sortBy),
+    [adventures, filter, filterProperty, selectedTags, sortBy]
+  )
 
   const searchForDatePeriod = (datePeriod) => {
     adventureService.findByPeriod(datePeriod).then((gotAdventures) => setAdventures(gotAdventures))
@@ -31,28 +33,9 @@ const Adventures = () => {
   }
 
   const handleSortFilterChange = (e) => {
-    switch (e.target.value) {
-      case 'name': {
-        setAdventures([...adventures.sort((a, b) => a.name.localeCompare(b.name))])
-        break
-      }
-      case 'address': {
-        setAdventures([...adventures.sort((a, b) => a.address.localeCompare(b.address))])
-        break
-      }
-      case 'rating': {
-        setAdventures([...adventures.sort((a, b) => b.ratingAverage - a.ratingAverage)])
-        break
-      }
-      case 'price': {
-        setAdventures([...adventures.sort((a, b) => a.price - b.price)])
-        break
-      }
-      default: {
-        break
-      }
-    }
+    setSortBy(e.target.value)
   }
+
   const updateTagFilters = (tag) => {
     selectedTags.includes(tag)
       ? setSelectedTags(selectedTags.filter((t) => t !== tag))
@@ -62,12 +45,13 @@ const Adventures = () => {
   return (
     <>
       <AllEntities
-        entities={adventuresToShow}
+        entities={filteredAndSortedEntities}
         handleSearchFieldChange={(e) => handleSearchFieldChange(e)}
         handleSearchFilterChange={(e) => handleSearchFilterChange(e)}
         handleSortFilterChange={(e) => handleSortFilterChange(e)}
         searchForDatePeriod={(datePeriod) => searchForDatePeriod(datePeriod)}
         updateTagFilters={updateTagFilters}
+        sortFilterItems={sortFilterItems}
       />
     </>
   )
