15e5350508738bb2b3606df71eb0048657f342ed
gmb <gameboyt7@gmail.com>
2022-09-06 23:34:28 +0200

    Removed unused constants

--- src/test/java/com/tim23/fishnchill/constants/CottageConstants.java
+++ src/test/java/com/tim23/fishnchill/constants/CottageConstants.java
@@ -4,7 +4,6 @@ public class CottageConstants {
     public static final String NEW_NAME = "Vikendaja";
     public static final String NEW_ADDRESS = "Mise Dimitrijevica 22, Novi Sad";
     public static final String NEW_DESCRIPTION = "new description";
-    public static final Long NEW_ID = 4L;
 
     public static final Long DB_ID = 1L;
     public static final String DB_NAME = "Cottage 1";
@@ -12,15 +11,5 @@ public class CottageConstants {
     public static final String DB_DESCRIPTION = "Lorem ipsum";
     public static final Long DB_OWNER_ID = 2L;
 
-    //number of students whose last name is DB_LASTNAME_COUNT
-    public static final String DB_LASTNAME_COUNT = "Stević";
-    public static final int DB_COUNT_WITH_ADDRESS = 2;
-
-    public static final Long DB_ID_REFERENCED = 1L;
-    public static final Long DB_ID_TO_DELETE = 3L;
-
-    public static final int DB_COUNT_STUDENT_EXAMS = 2;
-
     public static final int DB_COUNT = 4;
-    public static final int PAGE_SIZE = 3;
 }
